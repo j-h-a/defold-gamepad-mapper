c1b39225770e26957371f9bd549ca7db6b504606
Jay Abbott <1160012+j-h-a@users.noreply.github.com>
2025-01-08 21:38:18 +0000

    Ensure consistent output order and include raw input option

--- main/gamepads_pb.lua
+++ main/gamepads_pb.lua
@@ -12,13 +12,64 @@ local function get_platform()
     return platform_map[sys.get_sys_info().system_name] or "unknown"
 end
 
+local output_order = {
+    "GAMEPAD_LSTICK_LEFT",
+    "GAMEPAD_LSTICK_RIGHT",
+    "GAMEPAD_LSTICK_DOWN",
+    "GAMEPAD_LSTICK_UP",
+    "GAMEPAD_LSTICK_CLICK",
+    "GAMEPAD_RSTICK_LEFT",
+    "GAMEPAD_RSTICK_RIGHT",
+    "GAMEPAD_RSTICK_DOWN",
+    "GAMEPAD_RSTICK_UP",
+    "GAMEPAD_RSTICK_CLICK",
+    "GAMEPAD_LPAD_LEFT",
+    "GAMEPAD_LPAD_RIGHT",
+    "GAMEPAD_LPAD_DOWN",
+    "GAMEPAD_LPAD_UP",
+    "GAMEPAD_RPAD_LEFT",
+    "GAMEPAD_RPAD_RIGHT",
+    "GAMEPAD_RPAD_DOWN",
+    "GAMEPAD_RPAD_UP",
+    "GAMEPAD_LTRIGGER",
+    "GAMEPAD_LSHOULDER",
+    "GAMEPAD_RTRIGGER",
+    "GAMEPAD_RSHOULDER",
+    "GAMEPAD_START",
+    "GAMEPAD_BACK",
+    "GAMEPAD_GUIDE",
+    "GAMEPAD_RAW",
+}
+
 function M.new_driver(name, platform, dead_zone)
     local self = setmetatable({}, {__index = M})
     self.device = name
     self.platform = platform or get_platform()
     self.dead_zone = dead_zone or 0.1
-    self.maps = {}
+    self.maps = {
+        ["GAMEPAD_RAW"] = {
+            input = "GAMEPAD_RAW",
+            type = "GAMEPAD_TYPE_AXIS",
+            index = 0,
+            modifiers = {},
+        }
+    }
     return self
+end
+
+local function map_to_string(map)
+    local s = ("    map { input: %s type: %s index: %d "):format(map.input, map.type, map.index)
+
+    if map.type == "GAMEPAD_TYPE_HAT" and map.hat_mask then
+        s = s .. ("hat_mask: %d "):format(map.hat_mask)
+    end
+
+    for _, modifier in ipairs(map.modifiers) do
+        s = s .. ("mod { mod: %s } "):format(modifier)
+    end
+
+    s = s .. "}\n"
+    return s
 end
 
 function M:to_string()
@@ -26,18 +77,12 @@ function M:to_string()
     s = s .. ('    device: "%s"\n'):format(self.device)
     s = s .. ('    platform: "%s"\n'):format(self.platform)
     s = s .. ('    dead_zone: %.2f\n'):format(self.dead_zone)
-    for _, map in ipairs(self.maps) do
-        s = s .. ("    map { input: %s type: %s index: %d "):format(map.input, map.type, map.index)
 
-        if map.type == "GAMEPAD_TYPE_HAT" and map.hat_mask then
-            s = s .. ("hat_mask: %d "):format(map.hat_mask)
+    for _, input in ipairs(output_order) do
+        local map = self.maps[input]
+        if map then
+            s = s .. map_to_string(map)
         end
-
-        for _, modifier in ipairs(map.modifiers) do
-            s = s .. ("mod { mod: %s } "):format(modifier)
-        end
-
-        s = s .. "}\n"
     end
 
     s = s .. "}"
@@ -45,7 +90,7 @@ function M:to_string()
 end
 
 function M:add_map(map)
-    table.insert(self.maps, map)
+    self.maps[map.input] = map
 end
 
 return M
